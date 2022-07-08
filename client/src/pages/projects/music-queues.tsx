4952ddefa220650a4e208345d36e9690fc37d85a
ChinnyChilla <michaelzheng04@gmail.com>
2022-07-08 17:20:24 -0400

    finsih updateQueue api and create first track div

--- client/src/pages/projects/music-queues.tsx
+++ client/src/pages/projects/music-queues.tsx
@@ -1,10 +1,52 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 
-function testing() {
-	return (
-		<div>Hi</div>
-	)
+class MusicQueue extends Component {
+	public state: any = {
+		queue: undefined
+	}
+
+	renderFirstTrack(track: any) {
+		return(
+			<div>
+				<div id="currently-playing">
+					<div id='song-name-1'><span>{track.name}</span></div>
+					<div id='progress-bar'></div>
+					<div className='column left'>
+						<div id='info-box'>
+							<div className='column left grid'>
+								<div className='row'>
+									<span>Author</span>
+								</div>
+								<div className='row'>
+									<span>Source</span>
+								</div>
+							</div>
+							<div className='column right grid'>
+								<div className='row'>
+									<span>{track.author}</span>
+								</div>
+								<div className='row'>
+									<span>{track.source}</span>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className='column right'>
+
+					</div>
+				</div>
+			</div>
+		)
+	}
+	render() {
+		return(
+			<div>
+				<div id="Title"></div>
+			</div>
+		)
+	}
+
 }
 
-export default testing
+export default MusicQueue
--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const rateLimit = require('express-rate-limit')
 const fs = require('fs')
 const https = require('https')
 const ws = require('ws')
-const validator = require('validator')
+const validator = require('validator');
+const parseUrl = require('parse-url')
 require('dotenv').config()
 const app = express();
 
@@ -67,27 +68,44 @@ app.get('/api/get/downloadYoutubeVideo', async (req, res) => {
 	}
 })
 app.post('/api/post/updateQueue', async (req, res) => {
-
+	if (req.query.token !== procces.env.QUEUE_TOKEN) return res.sendStatus(401)
+	if (!req.query.id || !validator.isInt(req.query.id) || !req.query.queue) return res.sendStatus(400)
+	const queue = JSON.parse(req.query.queue)
+	serverQueues.set(req.query.id, queue)
+	
+	res.sendStatus(200)
+	wss.clients.forEach(ws => {
+		if (ws.queueId != req.query.id) return
+		ws.send(JSON.stringify({category: 'queue', data: queue}))
+	})
 })
-wss.on('connection', (ws) => {
+wss.on('connection', (ws, inc_req) => {
+	const params = parseUrl("http://testing.com" + inc_req.url).query
 	ws.isAlive = true;
-
+	ws.queueId = params.id
+	ws.invalidId = false
 	ws.on('pong', () => {
 		ws.isAlive = true;
 	})
-	ws.on('message', (msg) => {
-		ws.send(`Hello! you said ${msg}`)
-	})
-	ws.send("Websocket connected")
+	ws.send(JSON.stringify({category: 'handshake', data: "Connection successful"}))
+	if (!validator.isInt(params.id)) {
+		ws.invalidId = true
+		ws.send(JSON.stringify({category: 'error', data: 'Invalid id'}))
+		setTimeout(() => {ws.terminate()}, 1)
+	}
+	if (!ws.invalidId) {
+		ws.send(JSON.stringify({category: 'queue', data: serverQueues.get(ws.queueid)}))
+	}
 })
 
 setInterval(() => {
 	wss.clients.forEach(ws => {
 		if (!ws.isAlive) return ws.terminate();
 		ws.isAlive = false
-		ws.ping(null, false, true)
+		ws.ping(null, false, () => {return true})
 	})
 }, 10000)
+
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
 });
