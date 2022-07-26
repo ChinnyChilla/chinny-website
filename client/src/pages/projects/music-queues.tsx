fef8fb5c40d7410d7bd357154d890e13429ca85f
ChinnyChilla <michaelzheng04@gmail.com>
2022-07-26 19:51:22 -0400

    finished basic queue info

--- client/src/pages/projects/music-queues.tsx
+++ client/src/pages/projects/music-queues.tsx
@@ -57,15 +57,14 @@ class MusicQueue extends Component<Props> {
 				return document.documentElement.style.setProperty('--progress-percent', percent.toString())
 			}
 			const difference = this.state.queue.timeSongFinish - timeNow.getTime() + 1000
-			if (difference < 0) {
+			if (difference < -200) {
 				this.setState({...this.state, progressClass: "complete-song"})
 				return document.documentElement.style.setProperty('--progress-percent', "0")
 			}
-			const str = (Math.abs(difference - this.state.queue.firstTrack.durationMS) / this.state.queue.firstTrack.durationMS).toString()
-			document.documentElement.style.setProperty('--progress-percent', str)
-
 			const contentstring = this.changeTimeFormat(this.state.queue.firstTrack.durationMS - difference)
 			this.setState({ ...this.state, progressContent: contentstring })
+			const str = (Math.abs(difference - this.state.queue.firstTrack.durationMS) / this.state.queue.firstTrack.durationMS).toString()
+			document.documentElement.style.setProperty('--progress-percent', str)
 		}
 		catch {
 		}
@@ -86,22 +85,41 @@ class MusicQueue extends Component<Props> {
 			return `${pad2(hours)}:${pad2(minutes)}:${pad2(seconds)}`
 		}
 	}
+
+	componentDidUpdate(prevProp: Props, prevState: any) {
+		if (prevState.queue !== this.state.queue) {
+			if (this.state.progressClass === "complete-song") {
+				this.setState({ ...this.state, progressClass: "end-song" })
+			}
+		}
+	}	
 	componentWillUnmount() {
 		clearInterval(this.state.progressInterval)
+	}
+	renderTitle() {
+		if (this.state.queue?.channelName === undefined) { return <div id="title">No Current Queue in this Server</div> }
+
+		return <div id="title">Music Queue for {this.state.queue.channelName}</div>
+
 	}
 	renderFirstTrack() {
 		const queue = this.state.queue
 		if (!queue|| queue.deleted) {return (<div></div>)}
 		const track = queue.firstTrack
+		var trackURL = track.url
 		var source = 'Other'
 		const YOUTUBE_REGEX = /http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-_]*)(&(amp;)?‌​[\w?‌​=]*)?/
 		const SPOTIFY_REGEX = /^(https: \/\/open.spotify.com\/user\/spotify\/playlist\/|spotify:user:spotify:playlist:)([a-zA-Z0-9]+)(.*)$/
 		if (YOUTUBE_REGEX.test(track.url)) {
 			source = "Youtube"
+			const timeNow = new Date();
+			const seconds = Math.floor((this.state.queue.firstTrack.durationMS - (this.state.queue.timeSongFinish - timeNow.getTime() + 1000)) / 1000)
+			trackURL = trackURL + `&t=${seconds}`
 		} else if (SPOTIFY_REGEX.test(track.url)) {
 			source = "Spotify"
 		}
 		return(<div>
+				<div className={queue.paused?"alert-info-active":"alert-info"}><span>INFO: This queue is currently paused</span></div>
 				<div id="currently-playing">
 					<div className={"progress-bar " + this.state.progressClass}>
 					</div>
@@ -119,39 +137,47 @@ class MusicQueue extends Component<Props> {
 							<div className="info-item"><b>Author</b></div>
 							<div className='info-item'>{track.author}</div>
 							<div className="info-item"><b>Source</b></div>
-							<div className='info-item'><a className='link' target="_blank" href={track.url} rel="noreferrer"><span><u>{source}</u></span></a></div>
+						<div className='info-item'><a className='link' target="_blank" href={trackURL} rel="noreferrer"><span><u>{source}</u></span></a></div>
 						</div>
 					</div>
 				</div>
 			</div>)
 	}
-	componentDidUpdate(prevProp: Props, prevState: any) {
-		if (prevState.queue !== this.state.queue) {
-			if (this.state.progressClass === "complete-song") {
-				this.setState({ ...this.state, progressClass: "end-song" })
-			}
-		}
-	}	
-	renderTitle() {
-		if (this.state.queue?.channelName === undefined) { return <div id="title">No Current Queue in this Server</div>}
-		
-		return <div id="title">Music Queue for {this.state.queue.channelName}</div>
-
+	
+	renderQueue() {
+		const queue = this.state.queue
+		if (!queue || queue.deleted) { return (<div></div>) }
+		const queueSongs = this.state.queue.tracks
+		return queueSongs.map((song: any, index: any) => {
+			return(
+				<div className="queue-song">
+					<div className="index">{index + 1}.</div>
+					<div className="column queue-column-left">
+						<div className="queue-thumbnail-parent">
+							<a className='link' target="_blank" href={song.url} rel="noreferrer"><img src={song.thumbnail} className="queue-thumbnail" alt="thumbnail" /></a>
+							</div>
+					</div>
+					<div className="column queue-column-right">
+						<a className='link' target="_blank" href={song.url} rel="noreferrer">
+							<div className="queue-title"><span>{song.title}</span></div>
+						</a>
+						<div className="queue-duration"><span>{this.changeTimeFormat(song.durationMS)}</span></div>
+					</div>
+				</div>
+			)
+		})
 	}
-	renderPage() {
-		return this.state.html
-	}
+	
 	render() {
 		return(
-			<div>
-				<div>
+			<>
 				{this.renderTitle()}
-				</div>
-				<div>
 				{this.renderFirstTrack()}
-
+				<div id="split-queue" style={{fontSize: '5vmin', textAlign: 'center'}}>The rest of the queue...</div>
+				<div id="queue">
+				{this.renderQueue()}
 				</div>
-			</div>
+			</>
 		)
 	}
 
